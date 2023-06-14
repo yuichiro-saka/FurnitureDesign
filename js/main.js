87d663af8dbe9bb87fba6ca7743e884e8ad8ecee
Yuichiro Sakamoto <black9raccoon@yahoo.co.jp>
2023-06-14 10:05:36 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+  $('.toggle_btn').on('click', function() {
+
+    if ($('#header').hasClass('open')) {
+
+      $('#header').removeClass('open');
+
+    } else {
+
+      $('#header').addClass('open');
+    }
+  });
+
+  $('#mask').on('click', function(){
+    
+    $('header').removeClass('open');
+  });
+});
